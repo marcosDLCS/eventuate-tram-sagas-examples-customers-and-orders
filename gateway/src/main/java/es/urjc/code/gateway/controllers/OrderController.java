180b63faa8d9c5562c59e00c2d110ff8e0af6342
marcosDLCS <marcos.dlcs@gmail.com>
2020-04-21 22:14:06 +0200

    Cleanup

--- gateway/src/main/java/es/urjc/code/gateway/controllers/OrderController.java
+++ gateway/src/main/java/es/urjc/code/gateway/controllers/OrderController.java
@@ -6,21 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/order_aggregates")
 public class OrderController {
 
-    private final OrderService orderService;
+    /*private final OrderService orderService;
 
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
 
     @GetMapping("/{id}")
-    public Mono<OrderDetails> getOrderDetails(@PathVariable String id) {
-        return orderService.getOrderDetails(id);
-    }
+    public OrderDetails getOrderDetails(@PathVariable String id) {
+        return orderService.getOrderDetails(id).block();
+    }*/
 
 }
